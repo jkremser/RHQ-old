6653293f3497ea29ad38cc00d63adf7eabd52827
Heiko W. Rupp <hwr@pilhuhn.de>
2012-10-18 21:50:59 +0200

    removed plugins

--- modules/plugins/alt-lang/alt-lang-test-plugin/src/main/resources/org/rhq/plugins/altlang/test/javascriptserver/operations.js
+++
@@ -1,8 +0,0 @@
-if (action.name == 'echo') {
-    echo();
-}
-
-function echo() {
-    msg = parameters.getSimple('msg').stringValue;
-    return org.rhq.core.pluginapi.operation.OperationResult(msg);
-}
