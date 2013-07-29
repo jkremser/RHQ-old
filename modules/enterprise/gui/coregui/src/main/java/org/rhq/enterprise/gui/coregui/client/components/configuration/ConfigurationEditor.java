d9b329447e1cc431ce0601f9e7772aeaf0b24f58
Jirka Kremser <jkremser@redhat.com>
2013-07-29 14:38:14 +0200

    If there is just one group definition in ConfigurationEditor, normal form is used instead SectionStack component.

--- modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/components/configuration/ConfigurationEditor.java
+++ modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/components/configuration/ConfigurationEditor.java
@@ -454,10 +454,10 @@ public class ConfigurationEditor extends EnhancedVLayout {
                 sectionStack.addSection(buildGroupSection(definition));
             }
 
-            if (groupDefinitions.size() > 1) {
+//            if (groupDefinitions.size() > 1) {
                 this.toolStrip = buildToolStrip(layout, sectionStack);
                 layout.addMember(toolStrip);
-            }
+//            }
             layout.addMember(sectionStack);
         }
 
