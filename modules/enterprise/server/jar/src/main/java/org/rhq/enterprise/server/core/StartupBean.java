705c15d24c86a52f3c655e3959cc9768d0461888
Stefan Negrea <snegrea@redhat.com>
2013-10-22 10:42:50 -0500

    Add code to update the storage session when credentials get update in the system settings table. Created a quartz job to trigger the refresh every 2 minutes.
    The process to refresh the session is as follows:
    1) Get new credentials from the database
    2) Create a new session with new credentials
    3) Replace existing session with newly created one
    4) Allow few minutes for existing session drainage
    5) Shutdown old session

--- modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/core/StartupBean.java
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/core/StartupBean.java
@@ -87,6 +87,7 @@ import org.rhq.enterprise.server.scheduler.jobs.DynaGroupAutoRecalculationJob;
 import org.rhq.enterprise.server.scheduler.jobs.PurgePluginsJob;
 import org.rhq.enterprise.server.scheduler.jobs.PurgeResourceTypesJob;
 import org.rhq.enterprise.server.scheduler.jobs.SavedSearchResultCountRecalculationJob;
+import org.rhq.enterprise.server.scheduler.jobs.StorageClusterCredentialsJob;
 import org.rhq.enterprise.server.scheduler.jobs.StorageClusterInitJob;
 import org.rhq.enterprise.server.scheduler.jobs.StorageClusterReadRepairJob;
 import org.rhq.enterprise.server.storage.StorageClientManagerBean;
@@ -739,6 +740,16 @@ public class StartupBean implements StartupLocal {
             }
         }
         
+        try {
+            // Storage cluster credentials refresh job
+            final long initialDelay = 1000L * 60 * 5;
+            final long interval = 1000L * 60 * 2;
+            schedulerBean.scheduleSimpleRepeatingJob(StorageClusterCredentialsJob.class, true, false, initialDelay,
+                interval);
+        } catch (Exception e) {
+            log.error("Cannot schedule storage cluster credentials refresh job.", e);
+        }
+
         try {
             String cronString = "0 30 0 ? * SUN *";  // every sunday starting at 00:30.
             schedulerBean.scheduleSimpleCronJob(StorageClusterReadRepairJob.class, true, true, cronString);
